8ceaf570dacd622917ca89d68c1ff56a484ecfed
DamonHD <d@hd.org>
2016-08-15 17:24:10 +0100

    TODO-897: simple driver

---
+++ javasrc/uk/org/opentrv/ETV/driver/ETVSimpleDriverNBulkInputs.java
@@ -0,0 +1,19 @@
+package uk.org.opentrv.ETV.driver;
+
+import java.io.File;
+import java.io.IOException;
+
+/**Simple driver from N bulk and HDD data to output files.
+ * An input directory is supplied,
+ * and the data files are read from particular names within that input directory.
+ * <p>
+ * An output directory is supplied
+ * and the output files will be written to specific files within that output directory.
+ */
+public final class ETVSimpleDriverNBulkInputs
+    {
+    public static void doComputation(final File inDir, final File outDir) throws IOException
+        {
+// TODO
+        }
+    }
