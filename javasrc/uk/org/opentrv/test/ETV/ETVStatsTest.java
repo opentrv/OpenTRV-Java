76dd0ff68b2bad140b04387169a6aa8032f03ffc
DamonHD <d@hd.org>
2016-08-31 16:24:51 +0100

    TODO-897: stats summary TDD

--- javasrc/uk/org/opentrv/test/ETV/ETVStatsTest.java
+++ javasrc/uk/org/opentrv/test/ETV/ETVStatsTest.java
@@ -30,6 +30,8 @@ import org.junit.Test;
 import uk.org.opentrv.ETV.ETVHouseholdGroupSimpleSummaryStats;
 import uk.org.opentrv.ETV.ETVHouseholdGroupSimpleSummaryStats.MeanAndPopSD;
 import uk.org.opentrv.ETV.ETVHouseholdGroupSimpleSummaryStats.SummaryStats;
+import uk.org.opentrv.ETV.ETVPerHouseholdComputation.ETVPerHouseholdComputationResult;
+import uk.org.opentrv.hdd.HDDUtil.HDDMetrics;
 
 /**Testing statistical computations for ETV. */
 public class ETVStatsTest
@@ -50,5 +52,36 @@ public class ETVStatsTest
             assertTrue(Double.isNaN(mp.pVariance));
             assertTrue(Double.isNaN(mp.pSD));
             }
+        }
+
+    /**Test for main features of an singleton final simple stats summary.
+     * All variance/SD should be zero,
+     * and all means the same as the input value.
+     */
+    @Test public void testSingletonSimpleStatsSummary() throws IOException
+        {
+        // Given a single representative household HDD metrics...
+        final ETVPerHouseholdComputationResult hcr = new ETVPerHouseholdComputationResult(){
+            @Override public Float getRatiokWhPerHDDNotSmartOverSmart() { return(1.3f); }
+            @Override public String getHouseID() { return("TheAvenue"); }
+            @Override public HDDMetrics getHDDMetrics() { return(new HDDMetrics(1.5f, 4.0f, 0.8f, 42)); }
+            };
+        final SummaryStats s = ETVHouseholdGroupSimpleSummaryStats.computeSummaryStats(1, Collections.singleton(hcr));
+        assertNotNull(s);
+        assertEquals(1, s.getAllHouseholdsCount());
+        assertEquals(1, s.getFinalHouseholdsCount());
+        assertEquals(42, s.getNormalDayCount());
+        for(final MeanAndPopSD mp : new MeanAndPopSD[]{s.getStatsOverRSquared(), s.getStatsOverSlope(), s.getStatsOverEfficacy()})
+            {
+            assertNotNull(mp);
+            assertTrue(!Double.isNaN(mp.mean));
+            assertTrue(!Double.isNaN(mp.pVariance));
+            assertTrue(!Double.isNaN(mp.pSD));
+            assertEquals(0.0, mp.pVariance, 0.01);
+            assertEquals(0.0, mp.pSD, 0.01);
+            }
+        assertEquals(0.8, s.getStatsOverRSquared().mean, 0.01);
+        assertEquals(1.5, s.getStatsOverSlope().mean, 0.01);
+        assertEquals(1.3, s.getStatsOverEfficacy().mean, 0.01);
         }
     }
